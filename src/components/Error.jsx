7cefb34e595127935e6fcb236c89a889fad21782
Sidharth Bhasin <sid21bhasin@gmail.com>
2024-01-30 22:54:45 +0530

    Error page added

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Error = () => {
+  const navigate = useNavigate()
+  const handleClick = () => {
+    navigate('/signup')
+  }
   return (
-    <div>Error</div>
+    <div className='flex'>
+      <h1>You need to login first</h1>
+      <button className='hero-btn' onClick={handleClick}>Go To SignUp page</button>
+    </div>
   )
 }
 
